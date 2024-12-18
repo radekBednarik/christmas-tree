b5c10d7d7e44478261a38af9b6378b18bfe83c2a
radekBednarik <radek.bednarik@stesena.com>
2024-12-18 18:01:34 +0100

    dev: start with very basics

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,16 +1,36 @@
 import "./style.css";
 import p5 from "p5";
-import { getViewportSize } from "./window";
+import { getCoords, getViewportSize } from "./window";
+
+new p5(sketch);
 
 function sketch(p: p5) {
 	p.setup = () => {
 		const window = getViewportSize();
+
 		p.createCanvas(window.width, window.height);
+		p.colorMode("hsb");
+		p.noLoop();
 	};
 
 	p.draw = () => {
 		p.background("#0D3B66");
+
+		drawGround(p);
 	};
 }
 
-new p5(sketch);
+function drawGround(p: p5) {
+	const startPoint = getCoords(0, 90);
+	const endPoint = getCoords(100, 90);
+
+	const coords = {
+		x1: startPoint.x,
+		y1: startPoint.y,
+		x2: endPoint.x,
+		y2: endPoint.y,
+	};
+
+	p.stroke("white");
+	p.line(coords.x1, coords.y1, coords.x2, coords.y2);
+}
--- src/window.ts
+++ src/window.ts
@@ -1,6 +1,30 @@
+/**
+ * Returns window size as `{x, y}` where
+ * `x` corresponds to `window.innerWidth` and
+ * `y` to `window.innerHeight`.
+ */
 export function getViewportSize() {
 	return {
 		width: window.innerWidth,
 		height: window.innerHeight,
+	};
+}
+
+/**
+ * Returns exact coordinates of `{x, y}` given provided
+ * relative coordinates with regard to window size.
+ */
+export function getCoords(relX: number, relY: number) {
+	if (0 > relX || 100 < relX || 0 > relY || 100 < relY) {
+		throw new Error(
+			"Coordinates are expected to be as percentages and must be within <0, 100> interval.",
+		);
+	}
+
+	const wCoords = getViewportSize();
+
+	return {
+		x: Math.floor((relX / 100) * wCoords.width),
+		y: Math.floor((relY / 100) * wCoords.height),
 	};
 }
